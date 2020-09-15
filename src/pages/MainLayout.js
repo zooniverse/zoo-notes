a31b65e7b4e8e5b7607b95389d228365cc9dbc94
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-09-15 22:11:55 +0100

    Add footer, update Zooniverse colours

--- src/pages/MainLayout.js
+++ src/pages/MainLayout.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Box, Header, Heading, Image } from 'grommet'
-import { withRouter, Link } from 'react-router-dom'
+import { Anchor, Box, Footer, Header, Heading, Image, Text } from 'grommet'
+import { withRouter } from 'react-router-dom'
 import styled, { css } from 'styled-components'
 
 import zooniverseLogo from 'images/zooniverse-logo-white-192.png'
@@ -8,14 +8,11 @@ import zooniverseLogo from 'images/zooniverse-logo-white-192.png'
 import { mergedTheme } from 'theme.js'
 
 const StyledHeader = styled(Header)`
-  background: ${mergedTheme.global.colors.brand};
+  background: ${mergedTheme.global.colors['neutral-2']};
   color: #fff;
 `
 
 function MainLayout ({ children }) {
-  
-  console.log(mergedTheme)
-  
   return (
     <>
       <StyledHeader pad="1rem" direction="row" justify="stretch">
@@ -31,6 +28,9 @@ function MainLayout ({ children }) {
       >
         { children }
       </Box>
+      <Footer direction="column">
+        <Text>Powered by <Anchor href="https://www.zooniverse.org">the Zooniverse</Anchor></Text>
+      </Footer>
     </>
   )
 }
