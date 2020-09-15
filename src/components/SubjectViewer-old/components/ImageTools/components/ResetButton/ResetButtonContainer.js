317d9fc53cd688fdbf46a621025388f4979712d4
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-09-15 13:04:02 +0100

    Remove old SubjectViewer

--- src/components/SubjectViewer-old/components/ImageTools/components/ResetButton/ResetButtonContainer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import AppContext from 'stores'
-import ResetButton from './ResetButton'
-
-export default function ResetButtonContainer() {
-  const store = React.useContext(AppContext)
-  return <ResetButton onClick={store.image.reset} />
-}
