92c483b1e51f14b9b6b0b00ae0de16aae57bfb05
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-09-15 21:57:08 +0100

    Add colours to Header

--- src/pages/MainLayout.js
+++ src/pages/MainLayout.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import { Box, Header, Heading, Image } from 'grommet'
 import { withRouter, Link } from 'react-router-dom'
-import zooniverseLogo from 'images/zooniverse-logo-black-192.png'
+import styled, { css } from 'styled-components'
+
+import zooniverseLogo from 'images/zooniverse-logo-white-192.png'
 // import zooniverseLogo from 'images/zooniverse-word-black.png'
+import { mergedTheme } from 'theme.js'
+
+const StyledHeader = styled(Header)`
+  background: ${mergedTheme.global.colors.brand};
+  color: #fff;
+`
 
 function MainLayout ({ children }) {
+  
+  console.log(mergedTheme)
+  
   return (
     <>
-      <Header pad="1rem" direction="row" justify="stretch">
+      <StyledHeader pad="1rem" direction="row" justify="stretch">
         <Box size="xsmall" flex={false} width="2rem" height="2rem">
           <Image alt="Zooniverse" src={zooniverseLogo} fit="contain" />
         </Box>
         <Heading size="small" flex={true} border={true} background="#cce">
           Zooniverse - Zoo Notes
         </Heading>
-        
-      </Header>
+      </StyledHeader>
       <Box
         pad='small'
       >
