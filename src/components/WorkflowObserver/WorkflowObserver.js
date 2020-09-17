b6554f52d97e29fc78736cc5b39d191b2e6b4275
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-09-17 21:08:01 +0100

    WorkflowObserver now records most recent Subjects

--- src/components/WorkflowObserver/WorkflowObserver.js
+++ src/components/WorkflowObserver/WorkflowObserver.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Box } from 'grommet'
+import { Anchor, Box, Text } from 'grommet'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import Pusher from 'pusher-js';
@@ -8,18 +9,32 @@ import Pusher from 'pusher-js';
 let pusher = null
 let channel = null
 const ZOONIVERSE_PUSHER_APP_KEY = '79e8e05ea522377ba6db'
+const MAX_SUBJECTS = 10
 
 const WorkflowObserver = function ({
   workflowId
 }) {
+  const [recentSubjects, setRecentSubjects] = React.useState([])
+  
+  
   function handleClassification (data) {
+    console.log('+++ data ', data)
     if (!data) return
     
     // if (data.workflow_id != workflowId) return  // Use loose comparison since we might be dealing with strings or numbers
     
     const subjectId = data.subject_ids && data.subject_ids[0]
+    if (!subjectId) return
     
-    console.log(subjectId)
+    const userId = data.user_id
+    
+    const recents = recentSubjects.slice()
+    const existingIndex = recents.indexOf(subjectId)
+    if (existingIndex >= 0) recents.splice(existingIndex, 1)
+    
+    recents.unshift(subjectId)
+    
+    setRecentSubjects(recents.slice(0, Math.min(recents.length, MAX_SUBJECTS)))
     
   }
   
@@ -37,12 +52,18 @@ const WorkflowObserver = function ({
       pusher.subscribe('panoptes')
     }
   })
-  
-  
 
   return (
     <Box>
-      {workflowId}
+      <ul>
+        {recentSubjects.map(subjectId => (
+          <li key={`subject-${subjectId}`}>
+            <Anchor href={`/view/workflow/${workflowId}/subject/${subjectId}`}>
+              <Text>{subjectId}</Text>
+            </Anchor>
+          </li>
+        ))}
+      </ul>
     </Box>
   )
 }
