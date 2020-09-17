a36c823a679bac52ff0b1581339096639a149bec
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-09-17 22:02:47 +0100

    Add instructions

--- src/pages/WorkflowPage.js
+++ src/pages/WorkflowPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Box, Heading, Text } from 'grommet'
+import { Box, Heading, Paragraph, Text } from 'grommet'
+import styled from 'styled-components'
 import { observer } from 'mobx-react'
 import { withRouter } from 'react-router-dom'
 import AppContext from 'stores'
 
 import WorkflowObserver from 'components/WorkflowObserver'
+
+const StyledParagraph = styled(Paragraph)`
+  max-width: 40rem;
+`
 
 function WorkflowPage ({ match }) {
   const store = React.useContext(AppContext)
@@ -18,11 +23,16 @@ function WorkflowPage ({ match }) {
   
   return (
     <Box>
-      <Heading as="h2" size="xsmall">Preparing Workflow</Heading>
+      <Heading as="h2" size="xsmall">Observing Classifications</Heading>
       {workflowId &&
-        <Text>
-          Workflow: {workflowId} ({store.workflow.asyncState}) {store.workflow.current && store.workflow.current.display_name}
-        </Text>
+        <>
+          <Text>
+            Workflow: {workflowId} ({store.workflow.asyncState}) {store.workflow.current && store.workflow.current.display_name}
+          </Text>
+          <StyledParagraph>
+            This page is now observing workflow {workflowId}, showing recent Classifications for that project. Leave this web page open while you work on the project on another web browser tab/window.
+          </StyledParagraph>
+        </>
       }
       
       <WorkflowObserver
