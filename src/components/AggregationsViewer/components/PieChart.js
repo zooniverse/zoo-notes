0b39264894d8970e78fc94aedaac5af10e47ba93
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2020-10-13 20:29:24 +0100

    Pie Charts and various SingleTask improvements (#20)
    * Simplify data in SingleTask to simplifiedData
    
    * Simplify Task text
    
    * Add separate PieChart component
    
    * Add percentages to SingleTask bar chart
    
    * Increase SingleTask answer text sizes when expanded
    
    * Fixed incorrect calculations with maxCount vs numClassifications, and add a sanity check for comparion numClassificaitons with the actual number of answers.
    
    * Fix footnote
    
    * Add functional SVG arcs for pie slices
    
    * Add pie colours
    
    * Spruce up piechart labels
    
    * Switch default tab for SingleTask

---
+++ src/components/AggregationsViewer/components/PieChart.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+let cursorPos = { x: 0, y: 0 }
+
+const SVG = styled.svg`
+  max-height: 50vh;
+`
+
+const R = 100
+const MIN_ANGLE_TO_SHOW_COUNT = (30 / 360) * 2 * Math.PI
+
+const PieChart = function ({
+  colours,
+  data,
+  totalCount,
+}) {
+  if (totalCount <= 0 || !data || data.length === 0) return null
+  
+  let totalAngle = 0
+  
+  return (
+    <SVG viewBox={`${R*-1.2} ${R*-1.2} ${R*2.4} ${R*2.4}`}>
+      <circle r={R} cx='0' cy='0' fill='#fff' stroke='#eee' strokeWidth='4' />
+      <g>
+        {data.map((dataItem, index) => {
+          
+          const angle = dataItem.count / totalCount * 2 * Math.PI
+          const startAngle = totalAngle
+          const midAngle = startAngle + angle / 2
+          const endAngle = startAngle + angle
+          const largeArcFlag = (angle > Math.PI) ? 1 : 0
+          const sweepFlag = 1  // Clockwise sweep!
+          
+          totalAngle += angle
+
+          if (dataItem.count <= 0) return null
+          
+          return (
+            <g key={`pie-slice-${index}`}>
+              <path
+                d={`M 0 0 L ${Math.cos(startAngle)*R} ${Math.sin(startAngle)*R} A ${R} ${R} 0 ${largeArcFlag} ${sweepFlag} ${Math.cos(endAngle)*R} ${Math.sin(endAngle)*R} Z`}
+                fill={(colours && colours[index]) || '#666666'}
+                stroke='#ffffff'
+              />
+              {(angle > MIN_ANGLE_TO_SHOW_COUNT) && (
+                <text
+                  alignmentBaseline='middle'
+                  fill='#ffffff'
+                  fontSize='0.5em'
+                  textAnchor='middle'
+                  x={Math.cos(midAngle)*R/2}
+                  y={Math.sin(midAngle)*R/2}
+                >
+                  {'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[index]}: {(dataItem.count / totalCount * 100).toFixed(1)}%
+                </text>
+              )}
+            </g>
+          )
+        })}
+      </g>
+    </SVG>
+  )
+}
+
+PieChart.propTypes = {
+  colours: PropTypes.arrayOf(PropTypes.string),
+  data: PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.string,
+    count: PropTypes.number,
+  })),
+  totalCount: PropTypes.number,
+}
+
+PieChart.defaultProps = {
+  colours: [],
+  data: [],
+  totalCount: 0,
+}
+
+export default PieChart
--- src/components/AggregationsViewer/components/SingleTask.js
+++ src/components/AggregationsViewer/components/SingleTask.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Box, Meter, Paragraph, Text } from 'grommet'
-import styled, { css } from 'styled-components'
+import { Box, Meter, Paragraph, Tab, Tabs, Text } from 'grommet'
+import styled from 'styled-components'
+
+import PieChart from './PieChart'
 
 function simplifyText (text) {
   return text
     .replace(/!\[[^\]]*\]\([^\)]*\)/g, '')  // Remove Markdown images
 }
 
-const FixedWidthText = styled(Text)`
-  width: 5em;
+const FixedWidthTextS = styled(Text)`
+  width: 2.5em;
+  overflow: auto;
+`
+
+const FixedWidthTextM = styled(Text)`
+  width: 4em;
   overflow: auto;
 `
 
@@ -18,6 +25,13 @@ const FixedHeightBox = styled(Box)`
     ? ''
     : 'overflow: auto; max-height: 50vh;'
   }
+`
+
+const TinyBox = styled(Box)`
+  flex: 0 0 auto;
+  height: 2em;
+  width: 2em;
+  overflow: hidden;
 `
 
 const SingleTask = function ({
@@ -43,40 +57,88 @@ const SingleTask = function ({
   }
   
   const maxCount = Object.values(reductionsData).reduce((max, cur) => Math.max(max, cur))
+  
+  let sanityCheck = 0
+  const summarisedData = answers.map((ans, index) => {
+    const count = reductionsData[index] || 0
+    sanityCheck += count
+    return {
+      label: simplifyText(ans.label),
+      count: count,
+    }
+  })
+  
   const summary = <Paragraph>This image has been classified by <b>{numClassifications}</b> people.</Paragraph>
-
+  const footnote = (sanityCheck !== numClassifications && expand)
+    ? <Text size='xsmall' margin='xsmall'>The number of classifications do not match the number of answers. There are many reasons for this - for example, a user might have submitted a classification with no answer.</Text>
+    : null
+  
+  const colours = summarisedData.map((dataItem, index) => {
+    const hue = index / summarisedData.length * 360
+    return `hsl(${hue}, 60%, 50%)`  // hsv(hue, 100%, 31%) matches Zooniverse's #00979d brand colour, but hsv(hue, 60%, 50%) is much better when differentiating between 6+ colours.
+  })
+  
   return (
     <Box>
       {summary}
       <FixedHeightBox background='#ffffff' expand={expand} pad='xsmall'>
-        <Paragraph flex={false}>{question}</Paragraph>
-        {answers.map((ans, index) => {
-          const label = simplifyText(ans.label)
-          const count = reductionsData[index] || 0
-  
-          return (
-            <Box
-              flex={false}
-              key={`answer-${index}`}
-              margin='xsmall'
-            >
-              <Text size='xsmall'>{label}</Text>
+        <Tabs>
+          <Tab title='Q&amp;A'>
+            <Paragraph flex={false}>{question}</Paragraph>
+            {summarisedData.map(({ label, count }, index) => (
               <Box
-                align='center'
-                direction='row'
-                gap='small'
+                flex={false}
+                key={`answer-${index}`}
+                margin='xsmall'
               >
-                <Meter
-                  flex='grow'
-                  max={maxCount}
-                  values={[{ value: count, color: 'accent-4' }]}
-                />
-                <FixedWidthText flex={false}>{count}</FixedWidthText>
+                <Text size={(expand) ? 'small' : 'xsmall'}>{label}</Text>
+                <Box
+                  align='center'
+                  direction='row'
+                  gap='small'
+                >
+                  <Meter
+                    flex='grow'
+                    max={maxCount}
+                    values={[{ value: count, color: 'accent-4' }]}
+                  />
+                  <FixedWidthTextS flex={false}>{count}</FixedWidthTextS>
+                  {(expand && numClassifications > 0) && (
+                    <FixedWidthTextM flex={false}>{(count / numClassifications * 100).toFixed(1)}%</FixedWidthTextM>
+                  )}
+                </Box>
               </Box>
-            </Box>
-          )
-        })}
+            ))}
+          </Tab>
+          <Tab title='Chart'>
+            <PieChart
+              colours={colours}
+              data={summarisedData}
+              totalCount={numClassifications}
+            />
+            {expand && (
+              <Box>
+                {summarisedData.map(({ label, count }, index) => (
+                  <Box key={`pie-key-${index}`} direction='row' align='center' gap='xsmall'>
+                    <TinyBox
+                      align='center'
+                      background={colours[index]}
+                      color='#fff'
+                      direction='column'
+                      size='xsmall'
+                      pad='xsmall'
+                    >
+                      <Text size='xsmall' color='#ffffff'>{'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[index]}</Text>
+                    </TinyBox>
+                    <Text size='xsmall'>{label}</Text>
+                  </Box>
+                ))}
+              </Box>
+            )}
+          </Tab>
+        </Tabs>
       </FixedHeightBox>
+      {footnote}
     </Box>
   )
 }
