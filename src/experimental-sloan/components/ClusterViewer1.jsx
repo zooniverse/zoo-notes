452fc8c65dfbd2947614311f9fc10d736fb5ed59
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-02-17 23:07:04 +0000

    Render main Subject as the central cell

--- src/experimental-sloan/components/ClusterViewer1.jsx
+++ src/experimental-sloan/components/ClusterViewer1.jsx
@@ -3,23 +3,26 @@ import styled from 'styled-components'
 
 const SVG = styled('svg')`
   border: 1px solid rgba(128, 128, 128, 0.5);
-  max-width: 100%;
+  max-width: 600px;
 `
 
 const STYLES = {
   AXIS: {
     STROKE: 'rgba(128, 128, 128, 0.5)'
   },
+  CELL: {
+    FILL: '#cccccc',
+    STROKE: '#444444',
+    STROKE_WIDTH: 5,
+  }
 }
-
-const CELL_SIZE = 200
 
 function ClusterViewer1 ({
   mainSubjectId = '',
   subjects = {},
   cluster = {},
-  width = 1000,
-  height = 1000,
+  width = 600,
+  height = 600,
 }) {
   if (!mainSubjectId || !subjects[mainSubjectId]) return null
 
@@ -44,8 +47,46 @@ function ClusterViewer1 ({
       >
         <line x1={-width/2} y1={0} x2={width/2} y2={0} stroke={STYLES.AXIS.STROKE} />
         <line y1={-width/2} x1={0} y2={width/2} x2={0} stroke={STYLES.AXIS.STROKE} />
+
+        <Cell
+          subject={subjects[mainSubjectId]}
+          cx={0} cy={0}
+        />
       </SVG>
     </Box>
+  )
+}
+
+function Cell ({
+  subject = {},
+  size = 100,
+  cx = 0,
+  cy = 0,
+}) {
+  const subjectId = subject.id
+  const imageUrl = subject.locations[0]['image/jpeg']
+
+  // TODO: determine Subject Image Size
+
+  return (
+    <g>
+      <mask id={`cell-subject-id-${subjectId}`}>
+        <circle cx={cx} cy={cy} r={size/2} fill='#fff'/>
+      </mask>
+      <circle
+        cx={cx} cy={cy} r={size/2}
+        fill={STYLES.CELL.FILL} stroke={STYLES.CELL.STROKE} strokeWidth={STYLES.CELL.STROKE_WIDTH}
+      />
+      <image
+        xlinkHref={imageUrl}
+        mask={`url(#cell-subject-id-${subjectId})`}
+        width={size}
+        height={size}
+        x={-size/2}
+        y={-size/2}
+      />
+    </g>
+
   )
 }
 
