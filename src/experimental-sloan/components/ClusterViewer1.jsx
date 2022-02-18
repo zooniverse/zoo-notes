06d8bb2b90f3ce4942f776c108a51784ac9d9a70
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2022-02-18 14:53:08 +0000

    SLOAN Grant Experiment (#58)
    * Add experimental section
    
    * Add prototype Subjects from Galaxy Zoo Weird and Wonderful
    
    * Add Clusters data and first ClusterViewer
    
    * Upgrade Grommet to use NameValueList
    
    * Add simple SVG
    
    * Render main Subject as the central cell
    
    * Render other Subjects in cluster
    
    * Add 2nd experiment. Add information on viewers
    
    * Add toggle for showing/hiding main galaxy
    
    * Allow main galaxy to be hidden in ClusterViewer. Update ClusterViewer styles

---
+++ src/experimental-sloan/components/ClusterViewer1.jsx
@@ -0,0 +1,129 @@
+import { Box, NameValueList, NameValuePair, Text } from 'grommet'
+import styled from 'styled-components'
+
+const DEFAULT_SIZE = 500
+
+const Container = styled(Box)`
+  box-shadow: 0.1rem 0.1rem 0.25rem #444;
+  border-radius: 1rem;
+`
+
+const SVG = styled('svg')`
+  border: 1px solid rgba(128, 128, 128, 0.5);
+  max-width: ${DEFAULT_SIZE}px;
+`
+
+const STYLES = {
+  AXIS: {
+    STROKE: 'rgba(128, 128, 128, 0.5)'
+  },
+  CELL: {
+    FILL: '#cccccc',
+    STROKE: '#444444',
+    STROKE_WIDTH: 5,
+  },
+  MAIN_CELL: {
+    STROKE: '#44cccc',
+  }
+}
+
+const CELL_SIZE = 100
+
+function ClusterViewer1 ({
+  mainSubjectId = '',
+  subjects = {},
+  cluster = {},
+  width = DEFAULT_SIZE,
+  height = DEFAULT_SIZE,
+}) {
+  const subjectsInCluster = Object.values(subjects).filter(sbj => cluster.subject_ids.includes(parseInt(sbj.id)))
+
+  return (
+    <Container
+      border={true}
+      pad='medium'
+      margin='small'
+    >
+      <NameValueList>
+        <NameValuePair name='num_selected'>
+          <Text>{cluster['num_selected']}</Text>
+        </NameValuePair>
+        <NameValuePair name='cluster_size'>
+          <Text>{cluster['cluster_size']}</Text>
+        </NameValuePair>
+        <NameValuePair name='total_subjects'>
+          <Text>{cluster['total_subjects']}</Text>
+        </NameValuePair>
+      </NameValueList>
+      <SVG
+        viewBox={`${-width/2} ${-height/2} ${width} ${height}`}
+      >
+        <g>
+          <line x1={-width/2} y1={0} x2={width/2} y2={0} stroke={STYLES.AXIS.STROKE} />
+          <line y1={-width/2} x1={0} y2={width/2} x2={0} stroke={STYLES.AXIS.STROKE} />
+        </g>
+
+        {subjectsInCluster.map((subject, index) => {  /* Place Subjects in the cluster around the main cell*/
+          const dist = CELL_SIZE
+          const angle = index / subjectsInCluster.length * 2 * Math.PI - 0.5 * Math.PI
+          const cx = dist * Math.cos(angle)
+          const cy = dist * Math.sin(angle)
+          return (
+            <Cell
+              subject={subject}
+              cx={cx} cy={cy} size={CELL_SIZE}
+            />
+          )
+        })}
+
+        {mainSubjectId && (
+          <Cell  /* Main cell is in the centre */
+            subject={subjects[mainSubjectId]}
+            cx={0} cy={0} size={CELL_SIZE}
+            type='main'
+          />
+        )}
+      </SVG>
+      <Text size='small'>ClusterViewer v1</Text>
+    </Container>
+  )
+}
+
+function Cell ({
+  subject = {},
+  size = 100,
+  cx = 0,
+  cy = 0,
+  type = '',
+}) {
+  const subjectId = subject.id
+  const imageUrl = subject.locations[0]['image/jpeg']
+
+  // TODO: determine Subject Image Size
+
+  return (
+    <g transform={`translate(${cx} ${cy})`}>
+      <mask id={`cell-subject-id-${subjectId}`}>
+        <circle cx={0} cy={0} r={size/2} fill='#fff'/>
+      </mask>
+      <circle
+        cx={0} cy={0} r={size/2}
+        fill={STYLES.CELL.FILL}
+        stroke={(type === 'main') ? STYLES.MAIN_CELL.STROKE : STYLES.CELL.STROKE}
+        strokeWidth={STYLES.CELL.STROKE_WIDTH}
+      />
+      <image
+        xlinkHref={imageUrl}
+        mask={`url(#cell-subject-id-${subjectId})`}
+        width={size}
+        height={size}
+        x={-size/2}
+        y={-size/2}
+      />
+    </g>
+
+  )
+}
+
+export { ClusterViewer1 }
+export default ClusterViewer1
---
+++ src/experimental-sloan/pages/ExperimentsPage.js
@@ -0,0 +1,94 @@
+import { useState } from 'react'
+import { Box, CheckBox, Heading, Paragraph } from 'grommet'
+import styled from 'styled-components'
+
+import ClusterViewer1 from '../components/ClusterViewer1'
+
+import subjects from '../data/subjects.json'
+import clusters from '../data/clusters.json'
+
+const ConstrainedParagraph = styled(Paragraph)`
+  max-width: 32rem;
+`
+
+const ConstrainedBox = styled(Box)`
+  max-width: 32rem;
+`
+
+function ExperimentsPage () {
+  const [ ex1_showMain, ex1_setShowMain ] = useState(true)
+  const [ ex2_showMain, ex2_setShowMain ] = useState(true)
+
+  return (
+    <Box>
+      <Heading as="h2" size="xsmall">Welcome to the SLOAN Grant Experiments page!</Heading>
+      <ConstrainedParagraph>
+        <b>What:</b> these components are "Cluster Viewers", able to view an astronomical
+        Subject in the context of the cluster of similar astronomical objects that it
+        exists in.
+      </ConstrainedParagraph>
+      <ConstrainedParagraph>
+        <b>Why:</b> we're grafting these experiments onto Zoo Notes because it's easier to
+        do so than to spin up a new repo and website. We fully intend to transfer any
+        successful experiments to a new home.
+      </ConstrainedParagraph>
+
+      <ConstrainedBox
+        border={true}
+        pad='small'
+        margin='small'
+      >
+        <Heading as="h3">Experiment 1</Heading>
+        <ConstrainedParagraph>
+          Let's, say, <b>show all galaxies similar to Galaxy X</b>.
+          This cluster has <b>5 galaxies</b>.
+          The galaxy in the centre is Galaxy X, the "main galaxy" to which the
+          other galaxies in the cluster are being compared to.
+        </ConstrainedParagraph>
+        <ClusterViewer1
+          mainSubjectId={ex1_showMain ? 61582335 : undefined}
+          subjects={subjects}
+          cluster={clusters['subject_id=61582335']}
+        />
+        <Box>
+          <CheckBox
+            checked={ex1_showMain}
+            label='Show main galaxy'
+            onChange={(e) => ex1_setShowMain(e.target.checked) }
+            size='small'
+          />
+        </Box>
+      </ConstrainedBox>
+
+      <ConstrainedBox
+        border={true}
+        pad='small'
+        margin='small'
+      >
+        <Heading as="h3">Experiment 2</Heading>
+        <ConstrainedParagraph>
+          How does this look like with more galaxies in the cluster?
+          (<b>TODO: spread large clusters into multiple rings.</b>)
+          Hex clustering?
+        </ConstrainedParagraph>
+        <ClusterViewer1
+          mainSubjectId={ex2_showMain ? 61511507 : undefined}
+          subjects={subjects}
+          cluster={clusters['subject_id=61511507']}
+        />
+        <Box>
+          <CheckBox
+            checked={ex2_showMain}
+            label='Show main galaxy'
+            onChange={(e) => ex2_setShowMain(e.target.checked) }
+            size='small'
+          />
+        </Box>
+      </ConstrainedBox>
+
+    </Box>
+  )
+}
+
+export { ExperimentsPage }
+export default ExperimentsPage
