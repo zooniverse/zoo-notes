566d15b264a1f1901b5bd299db85f48e108874fd
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2022-03-07 21:30:31 +0000

    SLOAN Grant Experiment - ClusterViewer v1 and Subject Inspector (#61)
    * Add ability to Select cells
    
    * Add ability to open/close Subject Inspector
    
    * SubjectInspector now lists metadata
    
    * Improve styles for ClusterViewer

--- src/experimental-sloan/components/ClusterViewer1.jsx
+++ src/experimental-sloan/components/ClusterViewer1.jsx
@@ -1,5 +1,8 @@
-import { Box, NameValueList, NameValuePair, Text } from 'grommet'
+import { useState } from 'react'
+import { Box, Button, NameValueList, NameValuePair, Text } from 'grommet'
+import { FormClose } from 'grommet-icons'
 import styled from 'styled-components'
+import stopEvent from '../helpers/stopEvent'
 
 const DEFAULT_SIZE = 500
 
@@ -11,11 +14,20 @@ const Container = styled(Box)`
 const SVG = styled('svg')`
   border: 1px solid rgba(128, 128, 128, 0.5);
   max-width: ${DEFAULT_SIZE}px;
+  background: #222222;
+`
+
+const SelectableCircle = styled('circle')`
+  cursor: pointer;
+`
+
+const SelectableImage = styled('image')`
+  cursor: pointer;
 `
 
 const STYLES = {
   AXIS: {
-    STROKE: 'rgba(128, 128, 128, 0.5)'
+    STROKE: 'rgba(255, 255, 255, 0.5)'
   },
   CELL: {
     FILL: '#cccccc',
@@ -24,10 +36,15 @@ const STYLES = {
   },
   MAIN_CELL: {
     STROKE: '#44cccc',
-  }
+  },
+  SELECTED_CELL: {
+    STROKE: '#00ffff',
+    STROKE_WIDTH: 10,
+  },
 }
 
 const CELL_SIZE = 100
+const DISTANCE_BETWEEN_CELLS = 110
 
 function ClusterViewer1 ({
   mainSubjectId = '',
@@ -36,7 +53,16 @@ function ClusterViewer1 ({
   width = DEFAULT_SIZE,
   height = DEFAULT_SIZE,
 }) {
+  const [selectedSubject, setSelectedSubject] = useState(undefined)
+
   const subjectsInCluster = Object.values(subjects).filter(sbj => cluster.subject_ids.includes(parseInt(sbj.id)))
+
+  const selectSubject = (subjectId) => {
+    const selectedSubject = Object.values(subjects).find(sbj => sbj.id === subjectId)
+    setSelectedSubject(selectedSubject)
+  }
+
+  const closeSubjectInspector = () => { selectSubject(undefined) }
 
   return (
     <Container
@@ -64,14 +90,17 @@ function ClusterViewer1 ({
         </g>
 
         {subjectsInCluster.map((subject, index) => {  /* Place Subjects in the cluster around the main cell*/
-          const dist = CELL_SIZE
+          const dist = DISTANCE_BETWEEN_CELLS
           const angle = index / subjectsInCluster.length * 2 * Math.PI - 0.5 * Math.PI
           const cx = dist * Math.cos(angle)
           const cy = dist * Math.sin(angle)
           return (
             <Cell
+              key={subject.id}
               subject={subject}
               cx={cx} cy={cy} size={CELL_SIZE}
+              isSelected={subject.id === selectedSubject?.id}
+              onSelect={selectSubject}
             />
           )
         })}
@@ -80,10 +109,16 @@ function ClusterViewer1 ({
           <Cell  /* Main cell is in the centre */
             subject={subjects[mainSubjectId]}
             cx={0} cy={0} size={CELL_SIZE}
+            isSelected={mainSubjectId === selectedSubject?.id}
+            onSelect={selectSubject}
             type='main'
           />
         )}
       </SVG>
+      <SubjectInspector
+        subject={selectedSubject}
+        onClose={closeSubjectInspector}
+      />
       <Text size='small'>ClusterViewer v1</Text>
     </Container>
   )
@@ -94,6 +129,8 @@ function Cell ({
   size = 100,
   cx = 0,
   cy = 0,
+  isSelected = false,
+  onSelect = () => {},
   type = '',
 }) {
   const subjectId = subject.id
@@ -101,18 +138,47 @@ function Cell ({
 
   // TODO: determine Subject Image Size
 
+  const onClick = (e) => {
+    onSelect(subjectId)
+    stopEvent(e)
+  }
+
+  const onKeyPress = (e) => {
+    if (e?.key === 'Enter') {
+      onSelect(subjectId)
+      stopEvent(e)
+    }
+  }
+
+  let stroke = STYLES.CELL.STROKE
+  if (type === 'main') stroke = STYLES.MAIN_CELL.STROKE
+  if (isSelected) stroke = STYLES.SELECTED_CELL.STROKE
+
+  const strokeWidth = (isSelected)
+    ? STYLES.SELECTED_CELL.STROKE_WIDTH
+    : STYLES.CELL.STROKE_WIDTH
+
   return (
-    <g transform={`translate(${cx} ${cy})`}>
+    <g
+      transform={`translate(${cx} ${cy})`}
+      tabIndex='0'
+      onClick={onClick}
+      onKeyPress={onKeyPress}
+    >
       <mask id={`cell-subject-id-${subjectId}`}>
-        <circle cx={0} cy={0} r={size/2} fill='#fff'/>
+        <circle
+          cx={0} cy={0} r={size/2}
+          fill='#fff'
+          strokeWidth={strokeWidth}
+        />
       </mask>
-      <circle
+      <SelectableCircle
         cx={0} cy={0} r={size/2}
         fill={STYLES.CELL.FILL}
-        stroke={(type === 'main') ? STYLES.MAIN_CELL.STROKE : STYLES.CELL.STROKE}
-        strokeWidth={STYLES.CELL.STROKE_WIDTH}
+        stroke={stroke}
+        strokeWidth={strokeWidth}
       />
-      <image
+      <SelectableImage
         xlinkHref={imageUrl}
         mask={`url(#cell-subject-id-${subjectId})`}
         width={size}
@@ -121,8 +187,56 @@ function Cell ({
         y={-size/2}
       />
     </g>
-
   )
+}
+
+function SubjectInspector ({
+  subject,
+  onClose,
+}) {
+  if (!subject) {
+    return (
+      <Box>
+        <Text>Click on a Subject to inspect it.</Text>
+      </Box>
+    )
+  }
+
+  const metadata = (!subject.metadata)
+    ? []
+    : Object.keys(subject.metadata).map(key => {
+      return {
+        key,
+        value: subject.metadata[key]
+      }
+    })
+
+  return (
+    <Box border={true} pad='small' margin={{ vertical: 'xsmall', horizontal: 'none' }}>
+      <Box direction='row'>
+        <Box flex={true}>
+          <Text weight='bold'>Subject {subject.id}</Text>
+        </Box>
+        <Button
+          a11yTitle='Close Subject Inspector'
+          icon={<FormClose />}
+          onClick={onClose}
+          plain={true}
+        />
+      </Box>
+      {metadata.length === 0 && <Text>This Subject has no metadata</Text>}
+      {metadata.length > 0 && (
+        <NameValueList>
+          {metadata.map(m => (
+            <NameValuePair key={m.key} name={m.key}>
+              <Text>{m.value}</Text>
+            </NameValuePair>
+          ))}
+        </NameValueList>
+      )}
+    </Box>
+  )
+
 }
 
 export { ClusterViewer1 }
---
+++ src/experimental-sloan/helpers/stopEvent.js
@@ -0,0 +1,8 @@
+export default function stopEvent (e) {
+  if (!e) return false
+  e.preventDefault && e.preventDefault()
+  e.stopPropagation && e.stopPropagation()
+  e.returnValue = false
+  e.cancelBubble = true
+  return false
+}
