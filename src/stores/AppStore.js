343d25134c5e4c02612957153fb8abc65cfe37c1
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-05-09 18:47:46 +0100

    Remove Auth

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -1,28 +1,16 @@
-import { flow, types } from 'mobx-state-tree'
+import { types } from 'mobx-state-tree'
 import { AggregationsStore } from './AggregationsStore'
-import { AuthStore } from './AuthStore'
 import { ImageStore } from './ImageStore'
 import { SubjectStore } from './SubjectStore'
 import { ViewerStore } from './ViewerStore'
 import { WorkflowStore } from './WorkflowStore'
 
 const AppStore = types.model('AppStore', {
-  initialised: types.optional(types.boolean, false),
   aggregations: types.optional(AggregationsStore, () => AggregationsStore.create({})),
-  auth: types.optional(AuthStore, () => AuthStore.create({})),
   image: types.optional(ImageStore, () => ImageStore.create({})),
   subject: types.optional(SubjectStore, () => SubjectStore.create({})),
   viewer: types.optional(ViewerStore, () => ViewerStore.create({})),
   workflow: types.optional(WorkflowStore, () => WorkflowStore.create({})),
-}).actions(self => {
-  const initialise = flow (function * initialise() {
-    yield self.auth.checkCurrent()
-    self.initialised = true
-  })
-
-  return {
-    initialise
-  }
 })
 
 export { AppStore }
